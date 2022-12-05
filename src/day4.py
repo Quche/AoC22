2ce94abd3757f947a23fdd1a926c826218e6402d
QuentinHuchet <huchetq@gmail.com>
2022-12-05 22:27:54 +0100

    end day4

--- src/day4.py
+++ src/day4.py
@@ -1,26 +1,44 @@
-c = 0
+def day4_1():
 
-with open('inputs/day4/day4.txt') as f:
+    c = 0
 
-    i = 0
-    for line in f.readlines():
+    with open('inputs/day4/day4.txt') as f:
 
-        line = line.rstrip().split(',')
-        pair = [s.split('-') for s in line]
+        for line in f.readlines():
 
-        l1 = int(pair[0][0])
-        l2 = int(pair[1][0])
-        r1 = int(pair[0][1])
-        r2 = int(pair[1][1])
+            line = line.rstrip().split(',')
+            pair = [s.split('-') for s in line]
 
-        if (l1 > r1):
-            print(f'{pair}: l1 is {pair[0][0]}; r1 is {pair[0][1]}: {i}')
+            l1 = int(pair[0][0])
+            l2 = int(pair[1][0])
+            r1 = int(pair[0][1])
+            r2 = int(pair[1][1])
 
-        p1_in_p2 = (l1 <= l2) & (r1 <= r2)
-        p2_in_p1 = (l2 <= l1) & (r2 <= r1)
+            p1_in_p2 = (l1 >= l2) & (r1 <= r2)
+            p2_in_p1 = (l1 <= l2) & (r1 >= r2)
 
-        if (p1_in_p2 | p2_in_p1):
-            c += 1
-        i += 1
+            if (p1_in_p2 | p2_in_p1):
+                c += 1
 
-    print(f'total overlappings: {c}')
+        print(f'total overlappings: {c}')
+
+def day4_2():
+
+    c = 0
+
+    with open('inputs/day4/day4.txt') as f:
+
+        for line in f.readlines():
+
+            line = line.rstrip().split(',')
+            pair = [s.split('-') for s in line]
+
+            r1 = [*range(int(pair[0][0]), int(pair[0][1]) + 1)]
+            r2 = [*range(int(pair[1][0]), int(pair[1][1]) + 1)]
+
+            if True in [r1[i] in r2 for i in range(len(r1))]:
+                c += 1
+
+        print(f'total overlappings: {c}')
+
+day4_2()
