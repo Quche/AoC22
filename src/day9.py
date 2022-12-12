fd8304e726a7945ae5fc5551f5ebd646f52be57b
QuentinHuchet <huchetq@gmail.com>
2022-12-12 20:12:15 +0100

    day9 ... WIP

--- src/day9.py
+++ src/day9.py
@@ -1,7 +1,8 @@
-H = [[0,0]]
+import math
+
 S = [0,0]
+H = [S]
 T = [S]
-i = 0
 
 def unique(my_list):
     unique_el = []
@@ -10,17 +11,18 @@ def unique(my_list):
             unique_el.append(l)
     return unique_el
 
+def dist(T,H):
+    return math.sqrt((T[0]+H[0])**2 + (T[1]+H[1])**2)
+
 
 with open('inputs/day9/test.txt') as f:
-    i += 1
-    for line in f.readlines():
 
+    for idx, line in enumerate(f.readlines()):
         move = line.rstrip().split(' ')
         for inc in range(int(move[1])):
 
             # Head
             if move[0] == 'R':
-                newH = H[-1]
                 H.append([H[-1][0]+1, H[-1][1]])
             elif move[0] == 'U':
                 H.append([H[-1][0], H[-1][1]+1])
@@ -28,14 +30,3 @@ with open('inputs/day9/test.txt') as f:
                 H.append([H[-1][0]-1, H[-1][1]])
             else:
                 H.append([H[-1][0], H[-1][1]-1])
-
-            # Tail
-            if (H[-2][1] != H[-1][1]) & (H[-2][0] != H[-1][0]):
-                pass
-            else:
-                T.append(H[-1])
-
-            print(H[-1],T[-1])
-
-unique_T = unique(T)
-print(f'Total number of distinct position of tail: {unique_T}')
